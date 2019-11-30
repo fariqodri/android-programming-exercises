8567ac4a8fb802a811acfdaeb49020fe5f92e994
fariqodri <arfixfari@gmail.com>
2019-11-30 14:00:20 +0700

    finish wifi

--- tutorial6/app/src/main/java/id/ac/ui/cs/mobileprogramming/fariqodriandana/tutorial6/MainActivity.kt
+++ tutorial6/app/src/main/java/id/ac/ui/cs/mobileprogramming/fariqodriandana/tutorial6/MainActivity.kt
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.mobileprogramming.fariqodriandana.tutorial6
 
+import android.content.Context
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,12 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: create logic for wifi connect
-
         equal.setOnClickListener {
             val baseNumber = base.text.toString().toDouble()
             val powerNumber = power.text.toString().toInt()
             calc_result.text = power(baseNumber, powerNumber).toString()
+        }
+
+        connect.setOnClickListener {
+            val wifiSSID = ssid.text
+            val wifiPassword = password.text
+
+            val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            wifi.isWifiEnabled = true
+
+            val wifiConfiguration = WifiConfiguration()
+            wifiConfiguration.SSID = String.format("\"%s\"", wifiSSID)
+            wifiConfiguration.preSharedKey = String.format("\"%s\"", wifiPassword)
+
+            val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val netId = wifiManager.addNetwork(wifiConfiguration)
+            wifiManager.disconnect()
+            wifiManager.enableNetwork(netId, true)
+            wifiManager.reconnect()
         }
     }
 
